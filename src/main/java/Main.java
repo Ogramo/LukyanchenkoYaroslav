319e2f2c6ecc3046bfec1c75740a6666a65ca32e
Yaroslav <yaroslaw200019@gmail.com>
2023-10-12 18:32:25 +0300

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
-public class Main {
-    public static void main(String[] args) {
+import com.java.additional.IO.Runner;
 
-    }
+public class Main {
+        public static void main(String[] args) {
+            Runner start = new Runner();
+            start.run(args);
+        }
 }
